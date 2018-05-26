c3e0ace1512e45b940af10067037872ca3824329
Mark-PC <670188307@qq.com>
2018-05-27 01:41:10 +0800

    add

---
+++ ES6 Modules规范/circle.js
@@ -0,0 +1,6 @@
+const PI = 3.14
+let radius = 5
+let getArea = (r) => PI * r * r
+export {
+    getArea as default
+}
---
+++ commJs规范/run.js
@@ -0,0 +1,3 @@
+var mod = require('./index')
+console.log(mod.sum(2, '2')) // NaN
+console.log(mod.sum(2, 2)) // 4
