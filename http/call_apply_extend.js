9f1b39c3d5bb4b6722832cc6cbcd268ee5b76eee
Mark-PC <670188307@qq.com>
2018-05-28 00:01:29 +0800

    addSigned-off-by: Mark-PC <670188307@qq.com>

---
+++ http/call_apply_extend.js
@@ -0,0 +1,26 @@
+function Pet(words){
+    this.words = words ;
+    this.speak = function (){
+        console.log(this.words)
+        console.log(this)
+    }
+}
+
+function Dog(words) {
+    Pet.call(this, words);
+}
+
+var dog = new Dog('Wang');
+
+dog.speak()
+
+
+
+
+
+
+
+
+
+
+
---
+++ http/callback.js
@@ -0,0 +1,14 @@
+function learn(something) {
+    console.log(something)
+}
+function we(callback,something) {
+    something += 'is coll';
+    callback(something)
+}
+we(learn,'Nodejs')
+
+we(function(something){
+    console.log(something)
+},'Jade')
+
+//回调
