bad8a257f1d0ad4c16b9e9c9cc958734e12f45b2
Mark-Pro <670188307@qq.com>
2018-07-03 15:48:07 +0800

    add

--- hello/test.js
+++ hello/test.js
@@ -7,3 +7,15 @@ process.on('exit', function (code) {
     console.log('about to exit with code: ' + code);
 });
 
+var url = require('url');
+
+console.log(url.parse('https://user:pass@host.com:8080/path/to/file?query=string#hash'));
+
+var path = require('path');
+// 解析当前目录:
+var workDir = path.resolve('.'); // '/Users/michael'
+
+// 组合完整的文件路径:当前目录+'pub'+'index.html':
+var filePath = path.join(workDir, 'pub', 'index.html');
+// '/Users/michael/pub/index.html'
+console.log(filePath);
